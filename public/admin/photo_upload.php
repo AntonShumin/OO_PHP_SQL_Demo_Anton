95a387bd6df10de3f6c44c09ae46b357380419f7
AntonShumin <AntonShuminBe@gmail.com>
2016-06-20 13:07:22 +0200

    test class is not photo_upload

--- public/admin/photo_upload.php
+++ public/admin/photo_upload.php
@@ -4,13 +4,14 @@ require_once("../../includes/initialize.php");
 $message ="test";
 if(isset($_POST['submit'])) {
     $photo = new Photograph();
-    $photo->id = 1;
+    
     $photo->caption = $_POST['caption'];
     $photo->attach_file($_FILES['file_upload']);
     if($photo->save()) {
         $message = "Photo uploaded successfully";
     } else {
         $message = join("<br/>",$photo->errors);
+        //$message = "not working";
     }
 }
 ?>
@@ -21,7 +22,7 @@ if(isset($_POST['submit'])) {
     <body style="background-color:white;">
         <h2>Foto upload test</h2>
         <?php echo $message; ?>
-        <form action="test.php" enctype="multipart/form-data" method="post">
+        <form action="test.php"  method="post">
             <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
             <p> <input type="file" name="file_upload" /> </p>
             <p>Caption: <input type="text" name="caption" value="" /> </p>
